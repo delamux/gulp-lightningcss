bffa33a653586f3b8d0d3056fb2274314e8e9782
Luis del Amo <luisdelamo@gmail.com>
2023-04-01 19:39:05 +0200

    implemented brwoserlist, setter and transform finis

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const lightningcss = require('lightningcss');
+import browserslist from 'browserslist';
+const { transform, browserslistToTargets } = require('lightningcss');
 const path = require('path');
 const replaceExt = require('replace-ext');
 const PluginError = require('plugin-error');
@@ -6,9 +7,26 @@ const applySourceMap = require('vinyl-sourcemaps-apply');
 const { Transform } = require('stream');
 
 const PLUGIN_NAME = 'gulp-lightningcss';
+let browsersListVersions = '>= 0.25%';
+
+/**
+ * e.g: '>= 0.25%' means the las two versions
+ * Invoke this functions first before call gulpLightningcss
+ * You can also pass the targets as an object or using browserlist
+ * https://lightningcss.dev/transpilation.html#browser-targets
+ *
+ * @param {string} version
+ */
+const setBrowserlistVersions = (version) => {
+  browsersListVersions = version;
+};
+
+const getBrowserListVersions = () => browsersListVersions;
+
+let targets = browserslistToTargets(browserslist(getBrowserListVersions()));
 
 function replaceExtension(fp) {
-  return path.extname(fp) ? replaceExt(fp, '.js') : fp;
+  return path.extname(fp) ? replaceExt(fp, '.css') : fp;
 }
 
 /**
@@ -18,9 +36,10 @@ function replaceExtension(fp) {
  * @param {boolean} opts.sourceMap
  * @param {object | string} opts.targets
  * @param {string} opts.fileName
+ * @param {object} opts.targets
  * @returns
  */
-module.exports = function (opts) {
+function gulpLightningcss(opts) {
   opts = opts || {};
 
   return new Transform({
@@ -32,54 +51,46 @@ module.exports = function (opts) {
       }
 
       if (file.isStream()) {
-        cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
-        return;
+        this.emit(
+          'error',
+          new PluginError(PLUGIN_NAME, error, {
+            fileName: file.path,
+            showProperties: false,
+          })
+        );
       }
 
-      // const fileOpts = Object.assign({}, opts, {
-      //   filename: file.path,
-      //   sourceMaps: Boolean(file.sourceMap),
-      //   caller: Object.assign({ name: PLUGIN_NAME }, opts.caller),
-      // });
-
-      const { code } = lightningcss.transform({
+      const options = {
+        targets,
+        code: Buffer.from(file.contents.toString()),
         filename: file.path,
+        sourceMap: false,
+        minify: true,
         ...opts,
-      });
+      };
 
-      // .transform(file.contents.toString(), fileOpts)
-      // .then((res) => {
-      //   if (res) {
-      //     if (file.sourceMap && res.map) {
-      //       const sourcemaps = JSON.parse(res.map);
-      //       sourcemaps.file = replaceExtension(file.relative);
-      //       sourcemaps.sources = sourcemaps.sources.map((filePath) => {
-      //         return file.path === filePath
-      //           ? replaceExtension(file.relative)
-      //           : replaceExtension(path.relative(file.path, filePath));
-      //       });
-      //       applySourceMap(file, sourcemaps);
-      //     }
+      const { code, map } = transform(options);
 
-      //     file.contents = Buffer.from(res.code);
-      //     file.path = replaceExtension(file.path);
-      //   }
+      if (options.sourceMap && map) {
+        const sourcemaps = JSON.parse(map);
+        sourcemaps.file = replaceExtension(file.relative);
+        sourcemaps.sources = sourcemaps.sources.map((filePath) => {
+          return file.path === filePath
+            ? replaceExtension(file.relative)
+            : replaceExtension(path.relative(file.path, filePath));
+        });
+        applySourceMap(file, sourcemaps);
+      }
 
-      //   this.push(file);
-      // })
-      // .catch((error) => {
-      //   this.emit(
-      //     'error',
-      //     new PluginError(PLUGIN_NAME, error, {
-      //       fileName: file.path,
-      //       showProperties: false,
-      //     })
-      //   );
-      // })
-      // .then(
-      //   () => cb(),
-      //   () => cb()
-      // );
+      file.contents = Buffer.from(code);
+      file.path = replaceExtension(file.path);
+
+      this.push(file);
     },
   });
+}
+
+module.exports = {
+  gulpLightningcss,
+  setBrowserlistVersions,
 };
